c4cc238bfed43bf00e1cb58aad7a5de84de760c5
TVYanaE <yana.for.spam@gmail.com>
2025-11-21 22:01:03 +0100

    Config update: 2025-11-21 22:01:03

---
+++ nvim/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,16 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    --- @module "ibl"
+    --- @type ibl.config
+    opts = {
+        indent = {
+            char = "┊",
+        },
+        scope = {
+            enabled = true,
+            --show_start = true,
+            --show_end = true,
+        },
+    },
+}
