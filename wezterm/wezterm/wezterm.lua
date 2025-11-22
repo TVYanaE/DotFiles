444296446fbd53fc5cc11bbdd4bd9ff54412e484
TVYanaE <yana.for.spam@gmail.com>
2025-11-22 17:21:08 +0100

    Config update: 2025-11-22 17:21:08

---
+++ wezterm/wezterm/wezterm.lua
@@ -0,0 +1,46 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 11
+config.color_scheme = 'Dark Ocean (terminal.sexy)'
+
+
+-- Settings for background image 
+local dimmer = { 
+    brightness = 0.015,  
+    --saturation = 0.5,
+}
+
+config.background = {
+      {
+        source = { File = "/home/iana/.config/wezterm/background3.jpg" },
+        width = "Cover",
+        height = "Cover",
+        horizontal_align = "Center",  -- ок
+        vertical_align = "Middle",    -- вместо Center
+        opacity = 1,
+        hsb = dimmer,
+      }
+    
+}
+
+-- Settings for windows
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+-- Finally, return the configuration to wezterm:
+return config
