017f5d0fb89f203b2a083f2af42ed78404deaaa6
TVYanaE <yana.for.spam@gmail.com>
2025-11-22 20:57:09 +0100

    Config update: 2025-11-22 20:57:09

--- wezterm/wezterm/wezterm.lua
+++ wezterm/wezterm/wezterm.lua
@@ -6,6 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
 
+
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
@@ -34,13 +35,37 @@ config.background = {
     
 }
 
--- Settings for windows
+-- Settings for windows padding
 config.window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
 }
+
+config.window_decorations = "None"
+
+-- Turn off scrollbar
+config.enable_scroll_bar = false
+
+
+wezterm.on('format-tab-title', 
+    function(tab, tabs, panes, config, hover, max_width)
+    
+    local active_pane = tab.active_pane
+    local current_working_url = active_pane.current_working_dir
+    if current_working_url then
+        local working_path = current_working_url.path
+
+        local pattern = "([^/]+/?)$"
+        local working_dir = string.match(working_path, pattern)
+        
+        return {{ Text = working_dir }}
+    end
+
+    return {{ Text = 'test' }}
+    end
+)
 
 -- Finally, return the configuration to wezterm:
 return config
