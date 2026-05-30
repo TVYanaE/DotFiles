fc02956a13199c5009c274b9fc768164583b48ff
TVYanaE <yana.for.spam@gmail.com>
2026-05-30 17:36:24 +0200

    Config update: 2026-05-30 17:36:24

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,22 +6,32 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
 
+
+config.enable_wayland = false 
+
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 11
+config.font = wezterm.font_with_fallback {
+  "JetBrains Mono",
+  "Symbols Nerd Font Mono",
+}
+config.font_size = 13
 config.color_scheme = 'Dark Ocean (terminal.sexy)'
 
+config.text_background_opacity = 1.0
+
+-- Settings for background image 
 local dimmer = { 
-    brightness = 0.015,  
+    --brightness = 0.015,  
     --saturation = 0.5,
 }
 
 config.background = {
       {
-        source = { File = "/home/iana/.config/wezterm/background3.jpg" },
+        source = { File = "/home/iana/.config/wezterm/background_lizard.png" },
         width = "Cover",
         height = "Cover",
         horizontal_align = "Center",  -- ок
@@ -31,6 +41,38 @@ config.background = {
       }
     
 }
+
+-- Settings for windows padding
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+config.window_decorations = "None"
+
+-- Turn off scrollbar
+config.enable_scroll_bar = false
+
+
+wezterm.on('format-tab-title', 
+    function(tab, tabs, panes, config, hover, max_width)
+    
+    local active_pane = tab.active_pane
+    local current_working_url = active_pane.current_working_dir
+    if current_working_url then
+        local working_path = current_working_url.path
+
+        local pattern = "([^/]+/?)$"
+        local working_dir = string.match(working_path, pattern)
+        
+        return {{ Text = working_dir }}
+    end
+
+    return {{ Text = 'test' }}
+    end
+)
 
 -- Finally, return the configuration to wezterm:
 return config
